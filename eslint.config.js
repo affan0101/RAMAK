5c31dd5f0ff99901f5a50c089fe84630d2eccb26
affan0101 <ahmadaffan284@gmail.com>
2026-09-12 15:46:35 +0530

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  { ignores: ['dist/**', 'node_modules/**', 'reports/**'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,mjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest', sourceType: 'module',
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[_A-Z]', argsIgnorePattern: '^_' }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+];
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import '@fontsource-variable/manrope';
+import '@fontsource-variable/noto-sans-arabic';
+import './styles.css';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);

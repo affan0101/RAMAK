882547eacdeef741857dbae0b3f8c7d31b2088b5
Affan Ahmad <146713463+affan0101@users.noreply.github.com>
2026-09-12 21:12:34 +0530

    Refine Why RAMAK section imagery

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '@fontsource-variable/manrope'
 import '@fontsource-variable/noto-sans-arabic'
 import './styles.css'
+import './why-ramak.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
